0ce1c3c022a1f6e2bb42603bbdd704a834ef6d20
Alicja Raszkowska <malavarena@gmail.com>
2018-06-12 22:39:02 -0700

    Experiment: set up sockets.
    Set up sockets using Flask-SocketIO.
    Generate a new name on click, through the socket connection.
    Add frontend SocketIO code.

--- server/server.py
+++ server/server.py
@@ -1,14 +1,19 @@
 import os
 import random
-from flask import Flask
-from flask import render_template
-from flask import send_from_directory
-from flask import Response
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-text_file = open('/usr/share/dict/propernames', 'r')
-words = text_file.read().split('\n')
-text_file.close()
+app.config['SECRET_KEY'] = 'kitten'
+socketio = SocketIO(app)
+
+def seed_names(file_path):
+    text_file = open(file_path, 'r')
+    words = text_file.read().split('\n')
+    text_file.close()
+    return words
+
+words = seed_names('/usr/share/dict/propernames')
 log_path = os.path.join(app.root_path, 'static/log')
 
 @app.route('/')
@@ -24,20 +29,33 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/names')
-def feed_words():
-    def add_name(log_file_path):
-        log_file = open(log_file_path, 'a+')
-        log_file.write(random.choice(words) + '\n')
-        log_file.close()
-    
-    def get_names(log_file_path):
-        log_file = open(log_file_path, 'r')
-        log_data = log_file.read().split('\n')
-        log_file.close()
-        return log_data
-    
-    add_name(log_path)
-    log_data = get_names(log_path)
+def add_name(log_file_path):
+    log_file = open(log_file_path, 'a+')
+    name = random.choice(words)
+    log_file.write(name + '\n')
+    log_file.close()
+    return name
 
-    return render_template('words.html', log_data = log_data)
+def get_names(log_file_path):
+    log_file = open(log_file_path, 'r')
+    log_data = log_file.read().split('\n')
+    log_file.close()
+    return log_data
+
+@app.route('/names')
+def show_words():   
+    return render_template('words.html', log_data = get_names(log_path))
+
+@socketio.on('connect')
+def connected():
+    print('connection established')
+
+@socketio.on('getname', namespace="/names")
+def give_name(message):
+    print('I\'ll get you a name')
+    name = add_name(log_path)
+    print(name)
+    emit('name', { 'data': name })
+
+if __name__ == '__main__':
+    socketio.run(app)
